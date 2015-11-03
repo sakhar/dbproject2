941cd8a776d2208d4d732f7713bd747bf3cfaec3
sakhar <sa3147@columbia.edu>
2015-11-03 18:25:00 -0500

    Part 1 done (exactly the same as the reference code) Part 2A done

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 __author__ = 'sakhar'
 
-x = {'a':1, 'b':2}
-y = {'a':2,'c':3, 'd':6}
-print y
-y.update(x)
-print len(y)
+from bs4 import BeautifulSoup
+import urllib
+
+url = 'http://www.cs.columbia.edu/~gravano/cs6111/proj2.html'
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html,"html.parser")
+
+# Remove scripts
+for script in soup(["script", "style"]):
+    script.extract()
+text = soup.get_text()
+
+print text
